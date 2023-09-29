86fd8f1756d4f42c0d1e5246eef6607d01546016
rohit-ash <51155103+rohit-ash@users.noreply.github.com>
2023-09-29 23:30:04 +0530

    Delete GA_Power.m

--- GA_Power.m
+++
@@ -1,4 +0,0 @@
-nvars = 3;
-options = optimoptions('ga','ConstraintTolerance',1e-6,'PlotFcn', @gaplotbestf,'PopulationSize',100,'Generations',100,'EliteCount',2,'CrossoverFraction',0.8,'MutationFcn',@mutationadaptfeasible);
-rng(10,"twister")
-[x,fval] = ga(Power_Ensemble_func,nvars,[],[],[],[],[0 0 0],[1 1 1],[],options);
