751025ec0b0bc21701efb61be85f6faf1a479f57
rohit-ash <51155103+rohit-ash@users.noreply.github.com>
2023-09-29 23:38:09 +0530

    Delete Pareto_3D.m

--- Pareto_3D.m
+++
@@ -1,7 +0,0 @@
-fun = Func_Multiobj_Ensemble;
-options = optimoptions('paretosearch','UseVectorized',true,'ParetoSetSize',100,...
-    'PlotFcn','psplotparetof');
-lb = [0 0 0];
-ub = [1 1 1];
-rng default % For reproducibility
-[x,f] = paretosearch(fun,3,[],[],[],[],lb,ub,[],options);
