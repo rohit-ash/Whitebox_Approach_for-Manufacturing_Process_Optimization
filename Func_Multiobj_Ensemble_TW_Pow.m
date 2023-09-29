3fcb0f20c5389f27bdf651ae2a80bf69a90de4af
rohit-ash <51155103+rohit-ash@users.noreply.github.com>
2023-09-29 23:29:39 +0530

    Delete Func_Multiobj_Ensemble_TW_Pow.m

--- Func_Multiobj_Ensemble_TW_Pow.m
+++
@@ -1,7 +0,0 @@
-function multi_val = Func_Multiobj_Ensemble_TW_Pow()
-     Roughness = load('Ensemble_Roughness_hyperparam_optimized.mat');
-     ToolWear = load('Ensemble_ToolWear_hyperparam_optimized.mat');
-     %Power = load('Ensemble_Power_hyperparam_optimized.mat');
-     multi_val = @(x)[(Roughness.Model.predict(transpose(x'))*3.47 + 0.33),(ToolWear.Model.predict(transpose(x'))*0.52 + 0.03)];
-end
-%(Power.Model.predict(transpose(x'))*2.11 + 0.69)
